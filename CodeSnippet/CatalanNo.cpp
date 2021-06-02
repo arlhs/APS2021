1db5ee3b45ffb0856355647ae561af0df8071a53
Aditya Raj <adityaraj.lhs007@gmail.com>
2021-06-02 18:12:35 +0530

    Update CatalanNo.cpp

--- CodeSnippet/CatalanNo.cpp
+++ CodeSnippet/CatalanNo.cpp
@@ -1,25 +1,29 @@
-<include<bits/stdc++.h>
-using namsespace std;
-
-class Solution {
-  public:
-  
-    long long dyck(int n){
-        long long res = 1;
+ll Catalan(int n)
+    {
+        ll dp[n+1];
+        dp[0]=1;
         
-        for(int i=0; i<n; i++){
-            res = res*(2*n - i);
-            res = res/(i + 1);
+        for(int i = 1;i < n+1 ; ++ i) 
+            dp[i] = (((2*i)*(2*i-1)*dp[i-1])/(i*(i+1))); 
+
+        return dp[n];
+    }
+
+ll Catalan(int n) 
+    {
+        //code here
+        ll catalan[n+1];
+        catalan[0] = 1;
+        catalan[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            catalan[i]=0;
+            for(int j=0; j<i; j++)
+                catalan[i] += catalan[j]*catalan[i-j-1];
         }
         
-        return res/(n + 1);
-    }
-    
-    long long dyckPaths(int N) {
-        long long res=1;
+        // for(int i=0; i<=n; i++)
+        //     cout<<catalan[i]<<" ";
         
-        res = dyck(N);
-        
-        return res;
+        return catalan[n];
     }
-}
